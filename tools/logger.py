bd4316d8b6ff0978b067c7b1e861588660901159
Tanvir <tanvir.ahmed.tonoy@proton.me>
2025-01-06 21:04:42 +0600

    add additional logging levels and color codes to logger class

--- tools/logger.py
+++ tools/logger.py
@@ -1,12 +1,28 @@
+import datetime
+
+
 class Logger:
     @staticmethod
+    def _log(level, msg, color_code):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[{color_code}m[{timestamp}] {level}: {msg}\033[0m")
+
+    @staticmethod
     def error(msg):
-        print("\033[31m" + "ERROR: " + msg + "\033[0m")
+        Logger._log("ERROR", msg, "31")
 
     @staticmethod
     def info(msg):
-        print("\033[32m" + "INFO: " + "\033[0m" + msg)
+        Logger._log("INFO", msg, "32")
 
     @staticmethod
     def warning(msg):
-        print("\033[33m" + "WARN: " + msg + "\033[0m")
+        Logger._log("WARN", msg, "33")
+
+    @staticmethod
+    def debug(msg):
+        Logger._log("DEBUG", msg, "34")
+
+    @staticmethod
+    def critical(msg):
+        Logger._log("CRITICAL", msg, "35")
